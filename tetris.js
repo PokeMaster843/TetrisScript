14d445c049f75410cdf616f5fe18c5fef82236fb
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-20 08:47:40 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("tetrisCanvas");
 var ctx = canvas.getContext("2d");
 var blockSize = 8;
-ctx.fillRect(0, 0, 480, 600);
+ctx.lineWidth = "" + blockSize;
+ctx.rect(0, 0, 480, 600);
 
 var update = setInterval(function() {
   
