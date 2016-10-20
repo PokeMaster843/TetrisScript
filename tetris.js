3b3ecba55edfac49139656691a171cbba524ff58
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-20 13:05:25 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("tetrisCanvas");
 var ctx = canvas.getContext("2d");
 var spawnBlockIn = 60 * 10;
-var blockSize = 8;
+var blockSize = 16;
 var blockColors = ["#00ffff", "#000099", "#ff9933", "#ffff00", "#00ff00", "#cc0099", "#ff0000"];
 var blockShapes = ["00001111", "01000111", "00010111", "01100110", "00110110", "00100111", "11000110"];
 /* 
@@ -18,14 +18,14 @@ ctx.lineWidth = "1";
 var update = setInterval(function() {
   
   if(spawnBlockIn > 0) {
-    spawnBlockIn -= 1;
+    spawnBlockIn -= 2;
   }
   
   else {
     
     spawnBlockIn = 60 * 10;
     ctx.rect(8 + i, 8, blockSize, blockSize);
-    i += 8;
+    i += 16;
     ctx.stroke();
     
   }
