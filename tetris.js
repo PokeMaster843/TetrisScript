c373e065fe4279798a030233bb84e1f91e31fc88
John Knickerbocker <PokeMaster843@gmail.com>
2016-10-21 06:47:40 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -3,11 +3,11 @@ var ctx = canvas.getContext("2d");
 var spawnBlockIn = 60 * 10;
 var blockSize = 16;
 var blockColors = ["#00ffff", "#000099", "#ff9933", "#ffff00", "#00ff00", "#cc0099", "#ff0000"];
-var blockShapes = ["00001111", "01000111", "00010111", "01100110", "00110110", "00100111", "11000110"];
+var blockShapes = ["11110000", "01000111", "00010111", "01100110", "00110110", "00100111", "11000110"];
 /* 
  * The blockShapes work like this:
- * first 4 digits up here  - 0000
- * last 4 digits down here - 1111
+ * first 4 digits up here  - 1111
+ * last 4 digits down here - 0000
 */
 var startX = 5, startY = 5, i = 0;
 ctx.lineWidth = "8";
@@ -26,7 +26,7 @@ var update = setInterval(function() {
     
     spawnBlockIn = 60 * 10;
     ctx.rect(8 + i, 8, blockSize, blockSize);
-    i += 16;
+    i += blockSize;
     ctx.stroke();
     
   }
